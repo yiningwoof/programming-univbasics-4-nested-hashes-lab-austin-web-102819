eb8c01a617d80e20ef16aa38d3410ea38a7693c0
Yining Wang <yining.wang@utexas.edu>
2019-10-18 22:24:42 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -60,13 +60,13 @@ end
 
 def adding_matz
 # add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
-# }
 
 	programmer_hash = 
  		{
+ 		    :yukihiro_matsumoto => {
+ 		      :known_for => "Ruby",
+ 		      :languages => ["LISP", "C"]
+ 		    },
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
